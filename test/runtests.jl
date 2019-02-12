0df09120282fdd7c2b5103b9ab0c418d87871254
Benoit Pasquier <pasquieb@uci.edu>
2019-02-12 17:17:06 +1100

    Fix - test `isapprox` for code coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -55,4 +55,18 @@ using HyperDualNumbers, LinearAlgebra, SparseArrays, SuiteSparse
         @test A * (Af \ y) ≈ y
         @test M * (Mf \ y) ≈ y
     end
+
+    # Check that isapprox is used in all the ways (for code coverage)
+    @testset "Test isapprox" begin
+        # Create a real-valued sparse matrix
+        A = sparse(randn(n, n))
+        # Create a hyperdual-valued sparse matrix
+        A2 = A .+ ε₁
+
+        @test A2 ≈ A
+        @test A ≈ A2
+        @test (1.0 + 0ε₁) ≈ 1.0
+        @test 1.0 ≈ (1.0 + 0ε₁)
+    end
+
 end
